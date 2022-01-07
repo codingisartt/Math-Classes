584b52e317b06c374e5744957d05ffad6a86b74f
Tubanur Balci <40777037+codingisartt@users.noreply.github.com>
2022-01-07 14:41:58 +0300

    Create: Math Classes file is added

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {//Math classes are static methods so we dont neet to generate an object.
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-100)); //absolute value
+        System.out.println(Math.cbrt(8));
+        System.out.println(Math.ceil(2.4));
+        System.out.println(Math.floor(1.99));
+        System.out.println(Math.log10(100));
+        System.out.println(Math.max(100,200));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.random()*100);
+        System.out.println(Math.round(10.49));//int
+        System.out.println(Math.rint(10.44));//double
+        System.out.println(Math.signum(-59)); // sign
+        System.out.println(Math.sqrt(81));
+        
+    }
+}
